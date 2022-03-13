ec7020c3a1c4423208a355e695a6cc4f9d8ee6a3
sunnyicodelabs <sunny@icodelabs.co>
2022-03-13 20:51:39 +0600

    added auth otp

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,9 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const crypto = require("crypto");
+const otpGenerator = require("otp-generator");
+const asyncHandler = require("express-async-handler");
 // const {validationResult} = require("express-validator")
 exports.signup = (req, res) => {
 
@@ -40,6 +44,8 @@ exports.signup = (req, res) => {
 };
 
 exports.signin = (req, res) => {
+  const phone = req.body && req.body.phone;
+   console.log("Hello Request", "phone is : ", phone && phone);
   User.findOne({ email: req.body.email }).exec((error, user) => {
     if (error) return res.status(400).json({ error });
     if (user) {
@@ -52,6 +58,8 @@ exports.signin = (req, res) => {
           token,
           user: { firstName, lastName,email, role,fullName,_id,signupAs,Phone},
         });
+
+
       }else{
         res.status(404).json({
           message: "Invalid Password",
@@ -62,6 +70,49 @@ exports.signin = (req, res) => {
     }
   });
 };
+
+
+exports.userSignRequest = asyncHandler(async (req, res, next) => {
+  const phone = req.body && req.body.phone;
+
+  console.log("Hello Request", "phone is : ", phone && phone);
+
+  const otp = otpGenerator.generate(6, {
+    alphabets: false,
+    upperCase: false,
+    specialChars: false,
+  });
+  const ttl = 5 * 60 * 1000; //5 Minutes in miliseconds
+  const expires = Date.now() + ttl; //timestamp to 5 minutes in the future
+  const data = `${phone}.${otp}.${expires}`; // phone.otp.expiry_timestamp
+  const hash = crypto.createHmac("sha256", key).update(data).digest("hex"); // creating SHA256 hash of the data
+  const fullHash = `${hash}.${expires}`; // Hash.expires, format to send to the user
+  // you have to implement the function to send SMS yourself. For demo purpose. let's assume it's called sendSMS
+  //   sendSMS(phone, `Your OTP is ${otp}. it will expire in 5 minutes`);
+
+  try {
+    const sendOTP = await axios({
+      method: "post",
+      url: "http://sms.netitbd.com/smsapi",
+      data: {
+        api_key: "C20008695f48e97b8a7d15.42542556",
+        senderid: "8809612436347",
+        type: "text",
+        scheduledDateTime: "",
+        msg: `BardharaMart OTP is : ${otp}. Do not Share. It will expire in 5 minutes`,
+        contacts: `88${phone}`,
+      },
+    });
+    console.log(sendOTP);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
+  res.json(fullHash);
+});
+
+
+
 exports.signout = (req, res) => {
   res.clearCookie('token');
   res.status(200).json({
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { signup, signin,signout} = require("../controllers/auth");
+const { signup, signin,signout,userSignRequest} = require("../controllers/auth");
 const { validateSignupRequest, isRequestValidated, validateSigninRequest} = require("../validators/auth");
 
 const router = express.Router();
 router.post("/signup", validateSignupRequest, isRequestValidated ,signup);
 router.post("/signin", validateSigninRequest, isRequestValidated, signin);
+router.post("/signinrequest", validateSigninRequest, isRequestValidated, userSignRequest);
 router.post("/signout" ,signout);
 // router.post('/profile', requireSignin, (req, res) => {
 //     res.status(200).json({user:'profile'});
