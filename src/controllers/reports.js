b2968e7c6f10a44e3748cde16b85d027b5b27918
sunnyicodelabs <sunny@icodelabs.co>
2022-03-20 22:21:14 +0600

    added post data

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -92,8 +92,8 @@ exports.createPost = async (req, res) => {
       console.log(err)
     })
 
-    // await newPostMessage.save();
-    // res.status(201).json(newPostMessage);
+    await newPostMessage.save();
+    res.status(201).json(newPostMessage);
 
   } catch (error) {
     res.status(409).json({ message: error });
