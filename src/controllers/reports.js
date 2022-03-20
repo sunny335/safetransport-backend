9c7dc85d8b7c308930c843e8be36b93427934e90
sunnyicodelabs <sunny@icodelabs.co>
2022-03-20 17:08:57 +0600

    fixed await error

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -60,6 +60,13 @@ exports.getPosts = (req, res) => {
 
 exports.createPost = async (req, res) => {
   const post = req.body;
+  const newPostMessage = new PostMessage({
+    ...post,
+    creator: req.userId,
+    createdAt: new Date().toISOString(),
+  });
+
+
 
   var notification = {
     "title": 'Title of notification',
@@ -67,36 +74,43 @@ exports.createPost = async (req, res) => {
   };
   var fcm_tockens = [];
   var notification_body = {
-    'notification':notification,
+    'notification': notification,
     'notification_ids': fcm_tockens
   }
-  const newPostMessage = new PostMessage({
-    ...post,
-    creator: req.userId,
-    createdAt: new Date().toISOString(),
-  });
-  fetch('https://fcm.googleapis.com/fcm/send', {
-    'method': 'POST',
-    'headers': {
-      'Authorization': 'key='
-        + 'AAAAQT-h5Ww:APA91bEuQDmggn98R-Ab9ulcNrEnGF_NeqHEp4bnpnP7XTzKPlsLZZ_gvC5wheeqhXi3yuoz0ane_ZaE2097HzIKCjtZCu84tRIp_FAIHOlM7GauF7EQ7boIAuS5L88iSMbwMYdWFWVs',
-      'Content-Type': 'application/json'
-    },
-    'body': JSON.stringify(notification_body)
-  }).then(() => {
-    res.status(200).send('Notification send Successfull')
-  }).catch((err) => {
-    res.status(400).send('something went wrong');
-    console.log('something went wrong')
-  })
 
-  try {
-    await newPostMessage.save();
-    res.status(201).json(newPostMessage);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
-    console.log(err);
-  }
+
+//   const sendOTP = await axios({
+//     method: "post",
+//     url: "https://fcm.googleapis.com/fcm/send",
+//     headers: {
+//       Authorization: 'key='
+//         + 'AAAAQT-h5Ww:APA91bEuQDmggn98R-Ab9ulcNrEnGF_NeqHEp4bnpnP7XTzKPlsLZZ_gvC5wheeqhXi3yuoz0ane_ZaE2097HzIKCjtZCu84tRIp_FAIHOlM7GauF7EQ7boIAuS5L88iSMbwMYdWFWVs',
+//       Content-Type: 'application/json'
+//  }
+//   });
+
+await fetch('https://fcm.googleapis.com/fcm/send', {
+  'method': 'POST',
+  'headers': {
+    'Authorization': 'key='
+      + 'AAAAQT-h5Ww:APA91bEuQDmggn98R-Ab9ulcNrEnGF_NeqHEp4bnpnP7XTzKPlsLZZ_gvC5wheeqhXi3yuoz0ane_ZaE2097HzIKCjtZCu84tRIp_FAIHOlM7GauF7EQ7boIAuS5L88iSMbwMYdWFWVs',
+    'Content-Type': 'application/json'
+  },
+  'body': JSON.stringify(notification_body)
+}).then(() => {
+  res.status(200).send('Notification send Successfull')
+}).catch((err) => {
+  res.status(400).send('something went wrong');
+  console.log('something went wrong')
+})
+
+try {
+  await newPostMessage.save();
+  res.status(201).json(newPostMessage);
+} catch (error) {
+  res.status(409).json({ message: error.message });
+  console.log(err);
+}
 };
 
 // exports.deletePost = async (req, res) => {
