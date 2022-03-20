4f1513a854867b06e2d7faa70841d9b7a725d368
sunnyicodelabs <sunny@icodelabs.co>
2022-03-20 16:55:10 +0600

    heroku restart

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -92,7 +92,6 @@ exports.createPost = async (req, res) => {
 
   try {
     await newPostMessage.save();
-
     res.status(201).json(newPostMessage);
   } catch (error) {
     res.status(409).json({ message: error.message });
