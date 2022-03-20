d9517e22c7a6d1fbff7c22fa01630b86e95beddc
sunnyicodelabs <sunny@icodelabs.co>
2022-03-20 15:36:01 +0600

    added firebase notification

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -61,11 +61,32 @@ exports.getPosts = (req, res) => {
 exports.createPost = async (req, res) => {
   const post = req.body;
 
+  var notification = {
+    "title": 'Title of notification',
+    'Text': 'Subtitle'
+  };
+  var fcm_tockens = [];
+  var notification_body = {
+    'notification_ids': fcm_tockens
+  }
   const newPostMessage = new PostMessage({
     ...post,
     creator: req.userId,
     createdAt: new Date().toISOString(),
   });
+  fetch('https://fcm.googleapis.com/fcm/send', {
+    'method': 'POST',
+    'headers': {
+      'Authorization': 'key='
+        + 'AAAAQT-h5Ww:APA91bEuQDmggn98R-Ab9ulcNrEnGF_NeqHEp4bnpnP7XTzKPlsLZZ_gvC5wheeqhXi3yuoz0ane_ZaE2097HzIKCjtZCu84tRIp_FAIHOlM7GauF7EQ7boIAuS5L88iSMbwMYdWFWVs',
+      'Content-Type': 'application/json'
+    },
+    'body': JSON.stringify(notification_body)
+  }).then(() => {
+    res.status(200).send('Notification send Successfull')
+  }).catch((err) => {
+    res.status(400).send('something went wrong');
+  })
 
   try {
     await newPostMessage.save();
@@ -73,6 +94,7 @@ exports.createPost = async (req, res) => {
     res.status(201).json(newPostMessage);
   } catch (error) {
     res.status(409).json({ message: error.message });
+    console.log(err);
   }
 };
 
